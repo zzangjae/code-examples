c5df140872dda38bec89cab064cecc1ec42a605b
zzangjae <dondegi5@gmail.com>
2025-01-29 08:45:08 +0900

    feat : thymeleaf view template 예제 추가

---
+++ spring/실전! 스프링 부트와 JPA 활용1/jpashop/src/main/java/jpabook/jpashop/HelloController.java
@@ -0,0 +1,15 @@
+package jpabook.jpashop;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!!!");
+        return "hello"; // return 반환하는 String 값 + .html == 화면 이름
+    }
+}
